4ff543de4ed1c1d8c96adcb0ca7b559876238c11
lxieyang <lxieyang@umich.edu>
2019-09-24 10:42:54 -0400

    update

--- week-05/to-do-exercise/main.js
+++ week-05/to-do-exercise/main.js
@@ -8,15 +8,6 @@ for (let i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
-// Click on a close button to hide the current list item
-const close = document.getElementsByClassName('close');
-for (let i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    const div = this.parentElement;
-    div.style.display = 'none';
-  };
-}
-
 // Add a "checked" symbol when clicking on a list item
 const list = document.querySelector('ul');
 list.addEventListener(
@@ -29,31 +20,13 @@ list.addEventListener(
   false
 );
 
-// Create a new list item when clicking on the "Add" button
+// TODO: Click on a close button to hide the current list item
+
+// TODO: Create a new list item when clicking on the "Add" button
 const addNewItem = () => {
-  const li = document.createElement('li');
-  const inputValue = document.getElementById('myInput').value;
-  const t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert('You must write something!');
-  } else {
-    document.getElementById('myUL').appendChild(li);
-  }
-  document.getElementById('myInput').value = '';
-
-  const span = document.createElement('SPAN');
-  const txt = document.createTextNode('x');
-  span.className = 'close';
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (let i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      const div = this.parentElement;
-      div.style.display = 'none';
-    };
-  }
+  alert('should add a new todo item');
 };
 
-window.addNewItem = addNewItem;
+window.addNewItem = addNewItem; // do not modify this line
+
+// TODO: restore the to do list state after browser tab refresh
