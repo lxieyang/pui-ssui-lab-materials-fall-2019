ad5404461a2e42c85d76969a5b299e14962bec02
lxieyang <lxieyang@umich.edu>
2019-10-09 01:44:23 -0400

    week 7

---
+++ week-07/simple-todo-list-react/src/App.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    todoList: [
+      {
+        content: 'Meet with Brad',
+        finished: false,
+      },
+      {
+        content: 'Go to Costco',
+        finished: true,
+      },
+      {
+        content: 'Hit the gym',
+        finished: true,
+      },
+      {
+        content: 'Remember to buy milk',
+        finished: false,
+      },
+      {
+        content: 'Finish Homework 6',
+        finished: false,
+      },
+    ],
+    newTodoContent: '',
+  };
+
+  todoContentInputHandler = event => {
+    this.setState({
+      newTodoContent: event.target.value,
+    });
+  };
+
+  addItemToList = () => {
+    if (this.state.newTodoContent === '') {
+      return;
+    }
+    let newTodoList = [...this.state.todoList];
+    newTodoList.push({
+      content: this.state.newTodoContent,
+      finished: false,
+    });
+
+    this.setState({
+      todoList: newTodoList,
+      newTodoContent: '',
+    });
+  };
+
+  removeItemFromList = (event, idx) => {
+    event.stopPropagation();
+    let newTodoList = [...this.state.todoList];
+    newTodoList.splice(idx, 1);
+    this.setState({
+      todoList: newTodoList,
+    });
+  };
+
+  switchCheckedStatus = idx => {
+    let newTodoList = [...this.state.todoList];
+    newTodoList[idx].finished = !newTodoList[idx].finished;
+    this.setState({
+      todoList: newTodoList,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="Header">
+          <h2>Simple To Do</h2>
+          <input
+            type="text"
+            value={this.state.newTodoContent}
+            onChange={e => this.todoContentInputHandler(e)}
+            placeholder="new to do..."
+          />
+          <span onClick={() => this.addItemToList()} className="AddNewToDoButton">
+            Add to my list
+          </span>
+        </div>
+
+        <ul>
+          {this.state.todoList.map((todoItem, idx) => {
+            return (
+              <li key={idx} onClick={() => this.switchCheckedStatus(idx)}>
+                {todoItem.content}
+                {todoItem.finished ? <div className="DoneIcon">Done!</div> : null}
+                <div className="DeleteIcon" onClick={e => this.removeItemFromList(e, idx)}>
+                  x
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
